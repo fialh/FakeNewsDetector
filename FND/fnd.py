826507907f38c67205c92e8106486f83c5c90d94
azizuddinuzair <azizuddinuzair@gmail.com>
2025-01-30 14:50:26 -0600

    Starting project commit

--- FND/fnd.py
+++ FND/fnd.py
@@ -1,1 +1,12 @@
+# Creators: Uzair Azizuddin, Firas Al Halaq, Christian Pimentel
+
+# Purpose of Project: The goal is to develop a system that can automatically 
+# classify news articles as real or fake based on their content. The process involves collecting data,
+# preprocessing it, training a machine learning model, and then evaluating the model's performance.
+
+
+
+
+print("Hello WOrld!");
+
 
